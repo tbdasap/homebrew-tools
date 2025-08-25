57595005221994c47f5ad6ca2ccde5880b4e62dc
Ganesan Sriram <5333949+tbdasap@users.noreply.github.com>
2025-08-25 10:06:14 -0700

    Fix SHA

--- Formula/sputavaak-nr.rb
+++ Formula/sputavaak-nr.rb
@@ -4,7 +4,7 @@ class SputavaakNr < Formula
   desc "Sputavaak NoiseReducer CLI"
   homepage "https://github.com/tbdasap/python-tools"
   url "https://github.com/tbdasap/python-tools/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "2a7f8cb6d29923289e5aec2adc9311fa53dc8350"
+  sha256 "93b28527b4b9d65eaf872852b4351156af29f875f8ca0400f14cc62498b83021"
   license "MIT"
 
   depends_on "python@3.12"
